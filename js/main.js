6e4287ee9659790fa8976405d455f537e89542ca
evgeniym69 <evgeniym69@gmail.com>
2018-06-07 13:42:59 +0300

    Test task

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*var rangeDesign = document.querySelector('.range-design');
+var rangeWeb = document.querySelector('.range-web');
+var rangeMarketing = document.querySelector('.range-marketing');
+var rangeValue1 = rangeDesign.value;
+var rangeValue1 = rangeWeb.value;
+var rangeValue1 = rangeMarketing.value;
+
+
+
+ rangeDesign.style.backgroundColor = "#03b1af";
+ rangeDesign.style.backgroundSize = rangeValue1;*/
